9489e64f3f39eb1cea4981032372b4f918ae6aeb
Amon-n <a.newe@campus.tu-berlin.de>
2025-12-15 18:00:57 +0100

    removed bad code and added is_apendable method

--- Sequence.py
+++ Sequence.py
@@ -7,34 +7,24 @@ class Sequence:
         self.pcp_queue : Queue[PcpTuple] = Queue()
         self.pcp_queue.put(start_tuple)
         self.missing : str = ""
+        self.topString : str = ""
+        self.bottomString : str = ""
         self.more_in_first : bool = True # True => more in first part of tuple
 
-    def get_queue(self):
-        return self.pcp_queue
-
-    def get_copy(self):
-        return copy.deepcopy(self)
-
+    # This Method can be called when a tuple is beeing added to the sequence so that the missing var is updated
     def __add_to_missing(self, pcp_tuple : PcpTuple):
-        f = pcp_tuple.pcp_tuple[0]
-        s = pcp_tuple.pcp_tuple[1]
-
-        if self.more_in_first:
-            f = self.missing + f
-        else:
-            s = self.missing + s
-
-        for i in range(min(len(f), len(s))):
-            pass
-
-        if len(f) > len(s):
-            self.missing = f[len(s):]
-        else:
-            self.missing = s[f:]
-
-    def is_appendable(self, pcp_tuple : PcpTuple):
         pass
-    
+
+    def is_appendable(self, pcp_tuple : PcpTuple) -> bool:
+        if self.more_in_first:
+            concatination = self.missing + pcp_tuple[1]
+            other_string = pcp_tuple[0]
+        else:
+            concatination = self.missing + pcp_tuple[0]
+            other_string = pcp_tuple[1]
+        shorter, longer = sorted((concatination, other_string), key=len)
+        return longer.startswith(shorter)
+
     def add_tupel(self, pcp_tuple : PcpTuple):
         pass
     
